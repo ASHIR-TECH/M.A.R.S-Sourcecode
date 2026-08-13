3e0b493e2201ca69789715ecf4c1d681c2ca98c0
Contractor-x <dada4ash@gmail.com>
2026-08-13 23:18:19 +0100

    updated layout.tsx

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,7 +5,6 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="sign-in" />
     </Stack>
   );
 }
