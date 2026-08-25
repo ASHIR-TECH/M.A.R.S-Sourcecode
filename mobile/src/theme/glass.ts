05fea0cb4ca4d25a805f8ba03f7e379138b8a51d
Contractor-x <dada4ash@gmail.com>
2026-08-25 18:21:03 +0100

    upddated glass

--- mobile/src/theme/glass.ts
+++ mobile/src/theme/glass.ts
@@ -3,6 +3,6 @@ import { BlurViewProps } from 'expo-blur';
 export const glass = {
   intensity: 70,
   tint: 'dark' as BlurViewProps['tint'],
-  borderColor: 'rgba(232,163,77,0.4)',
+  borderColor: 'rgba(215, 128, 30, 0.7)', /** this is where you change the color of padding button   */
   radius: 10,
 } as const;
