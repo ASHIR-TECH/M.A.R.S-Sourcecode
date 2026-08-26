f6c1e5b9f2e7ca605229dcdb2082c869086d0697
Contractor-x <dada4ash@gmail.com>
2026-08-26 21:40:14 +0100

    added mockdevices.ts

---
+++ mobile/src/data/mockDevices.ts
@@ -0,0 +1,18 @@
+import { Device } from '../types/device';
+
+export const mockDevices: Device[] = [
+  {
+    id: 'DEV-091',
+    name: 'CONTRACTOR',
+    os: 'Kali Linux',
+    status: 'online',
+    lastSeen: 'Just Now',
+  },
+  {
+    id: 'DEV-022',
+    name: 'WORK-LAPTOP',
+    os: 'Windows 11 Pro',
+    status: 'idle',
+    lastSeen: '14m ago',
+  },
+];
