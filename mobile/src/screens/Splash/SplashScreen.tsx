4627ef5abd4eb83e786855496b1b422fb8c4dd37
Contractor-x <dada4ash@gmail.com>
2026-08-27 15:34:37 +0100

    modified splashscreen.tsx

--- mobile/src/screens/Splash/SplashScreen.tsx
+++ mobile/src/screens/Splash/SplashScreen.tsx
@@ -22,6 +22,7 @@ export function SplashScreen({ onFinished }: SplashScreenProps) {
 
   const [fontsLoaded, fontError] = useFonts({
     'Audiowide-Regular': require('../../../assets/fonts/Audiowide-Regular.ttf'),
+    'Quantico-Bold': require('../../../assets/fonts/Quantico-Bold.ttf'),
   });
 
   useEffect(() => {
