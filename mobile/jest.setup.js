9e266a3201a0267ec3a15dfe009b3fc8c9402441
Contractor-x <dada4ash@gmail.com>
2026-09-01 20:56:00 +0100

    added jest.setup.js

--- mobile/jest.setup.js
+++ mobile/jest.setup.js
@@ -1,1 +1,19 @@
 require('react-native-reanimated').setUpTests();
+
+// Native modules that aren't present in the jest environment get stubbed so
+// any screen importing them remains testable (e.g. SupportScreen's shows,
+// SettingsScreen's tabs).
+jest.mock('react-native-webview', () => {
+  const React = require('react');
+  const { View } = require('react-native');
+  const MockWebView = React.forwardRef((props, ref) => React.createElement(View, { ...props, ref }));
+  return { WebView: MockWebView, default: MockWebView };
+});
+
+jest.mock('expo-video', () => {
+  const React = require('react');
+  const { View } = require('react-native');
+  const MockVideoView = (props) => React.createElement(View, props);
+  const useVideoPlayer = () => ({ play: jest.fn(), pause: jest.fn(), loop: false });
+  return { VideoView: MockVideoView, useVideoPlayer, createVideoPlayer: jest.fn() };
+});
