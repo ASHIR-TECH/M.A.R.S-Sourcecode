b0008e02d80cbcf5e4b735e7be9a187b24791ccb
Contractor-x <dada4ash@gmail.com>
2026-09-08 18:58:43 +0100

    updated app.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,46 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { Animated, StyleSheet, View } from 'react-native';
 import { SplashScreen } from './src/screens/Splash/SplashScreen';
 import { RootNavigator } from './src/navigation/RootNavigator';
 
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
+
 export default function App() {
-  const [splashDone, setSplashDone] = useState(false);
+  const [showSplash, setShowSplash] = useState(true);
+  const splashOpacity = useRef(new Animated.Value(1)).current;
+  const contentOpacity = useRef(new Animated.Value(0)).current;
 
-  if (!splashDone) {
-    return <SplashScreen onFinished={() => setSplashDone(true)} />;
-  }
+  const handleSplashFinished = () => {
+    Animated.timing(contentOpacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+    Animated.timing(splashOpacity, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start(({ finished }) => {
+      if (finished) {
+        setShowSplash(false);
+      }
+    });
+  };
 
-  return <RootNavigator />;
+  return (
+    <View style={styles.root}>
+      <Animated.View style={[StyleSheet.absoluteFill, { opacity: contentOpacity }]}>
+        <RootNavigator />
+      </Animated.View>
+      {showSplash ? (
+        <Animated.View style={[StyleSheet.absoluteFill, { opacity: splashOpacity }]}>
+          <SplashScreen onFinished={handleSplashFinished} />
+        </Animated.View>
+      ) : null}
+    </View>
+  );
 }
