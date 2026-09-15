5c514c240eaaab27c7ec293cd7794f5227b35cbf
Contractor-x <dada4ash@gmail.com>
2026-09-15 17:40:32 +0100

    updated homescreen.tsyles.ts

--- mobile/src/screens/Home/HomeScreen.styles.ts
+++ mobile/src/screens/Home/HomeScreen.styles.ts
@@ -5,9 +5,6 @@ import { spacing } from '../../theme/spacing';
 import { tabBarMetrics } from '../../navigation/TabNavigator.styles';
 
 export const styles = StyleSheet.create({
-  blurBg: {
-    flex: 1,
-  },
   container: {
     flex: 1,
   },
@@ -44,7 +41,6 @@ export const styles = StyleSheet.create({
     width: 36,
     height: 36,
     borderRadius: 18,
-    backgroundColor: 'rgba(255,255,255,0.1)',
     alignItems: 'center',
     justifyContent: 'center',
   },
